117dd5f8f94dbf989e49a7cf33072d3010fc8f0f
Christopher Hempel <chempel.work@gmail.com>
2024-08-26 16:43:29 -0400

    Anonymized file names

--- tables_to_graphs.py
+++ tables_to_graphs.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 
-table1_file = '/Users/simplexdna/Library/CloudStorage/GoogleDrive-christopher.hempel@kaust.edu.sa/.shortcut-targets-by-id/1c91orCwfstL7NmlFbhdJ8Ssz87pDMHx-/SIREN project/table1.xlsx'
+table1_file = 'table1.xlsx'
 
 df1 = pd.read_excel(table1_file, header=None, names=["metric", "Species count"])
 
